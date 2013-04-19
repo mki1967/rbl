3f7dce9af22ba1a2cff8adb36a8cb4c27d649f05
Marcin Kik <mki1967@gmail.com>
2013-04-19 13:09:55 +0200

    spell-checked html and updated year of release to 2013

--- svg-experiment/rbl.js
+++ svg-experiment/rbl.js
@@ -1,7 +1,7 @@
 /*
    
     RedBlueLines - simple stereoscopic editor to be emeded in self-repicable SVG web pages
-    Copyright (C) 2011  Marcin Kik mki1967@gmail.com
+    Copyright (C) 2013  Marcin Kik mki1967@gmail.com
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
